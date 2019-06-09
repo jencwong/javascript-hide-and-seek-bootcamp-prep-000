6bc8369e8bc8876134e8d63a1048c2553b0800e2
Jen W <jsc525@gmail.com>
2019-06-09 16:36:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ function nestedTarget(target){
 }
 
 function increaseRankBy(n){
-  return parseInt(.ranked-list)+n;
+  const rankedLists  = document.querySelectorAll('.ranked-list');
+  
+  for (let i = 0, l = rankedLists.length; i < l, i++){
+    let children = rankedLists[i].children
+    
+    for ()
+  }
+  
+  
+ 
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
 }
