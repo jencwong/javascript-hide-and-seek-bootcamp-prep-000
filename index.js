7e1dcda7e363c1abba5c4ccd7d1a01b03a002263
Jen W <jsc525@gmail.com>
2019-06-09 16:38:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(target){
 function increaseRankBy(n){
   const rankedLists  = document.querySelectorAll('.ranked-list');
   
-  document.getElementById('app').querySelectorAll('ul.ranked-list')
+  document.getElementById('app').querySelectorAll('ul.ranked-list');
 
    for (let i = 0; i < upRank.length; i++){
     upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
