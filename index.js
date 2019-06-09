d140bcf38d82c7b649328cfdb6970a9a3dd083c2
Jen W <jsc525@gmail.com>
2019-06-09 17:48:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target){
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  const rankedLists  = document.querySelectorAll('.ranked-list');
+  
+  document.getElementById('app').querySelectorAll('ul.ranked-list');
+  
+  for (let i = 0; i < rankedLists.length; i++){
+    rankedLists[i].innerHTML = ((parseInt(rankedLists[i].innerHTML)) + n);
+  }
+}
+
+function deepestChild(){
+ var deep = document.getElementById('grand-node').querySelectorAll('div');
+ return deep[deep.length-1];
+}
