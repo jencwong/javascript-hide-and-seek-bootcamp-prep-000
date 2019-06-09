e3a641ee33f8c95bf6961dd874e6f01421487499
Jen W <jsc525@gmail.com>
2019-06-09 16:42:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ function increaseRankBy(n){
   const rankedLists  = document.querySelectorAll('.ranked-list');
   
   document.getElementById('app').querySelectorAll('ul.ranked-list');
-
-   for (let i = 0; i < upRank.length; i++){
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
-    }
+  
+  for (let i = 0; i < rankedLists.length; i++){
+    rankedLists[i].innerHTML = ((parseInt(rankedLists[i].innerHTML)) + n);
+  }
+}
